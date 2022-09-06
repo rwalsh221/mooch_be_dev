1e7a17596eb1cfc6061209eb2af28251f7ded5f7
Richard Walsh <richwalsh2@googlemail.com>
2022-09-06 15:09:45 +0100

    update auth token

--- athlete/index.php
+++ athlete/index.php
@@ -2,7 +2,7 @@
 
 require dirname(__DIR__, 1) . '/includes/classes/curl/CurlSettings.php';
 
-$authToken = '86e487c768c0ce1630788d5f1ce1af11045ca2f4';
+$authToken = '3a38f64e0d147f1527fb805ea5739bd0cd2efd91';
 
 $getAthlete = new CurlSettings('/athlete', array(
     'Content-Type: application/json', 'Authorization: Bearer ' . $authToken));
--- athlete/stats/index.php
+++ athlete/stats/index.php
@@ -24,7 +24,7 @@
 
 require dirname(__DIR__, 2) . '/includes/classes/curl/CurlSettings.php';
 
-$authToken = '86e487c768c0ce1630788d5f1ce1af11045ca2f4';
+$authToken = '3a38f64e0d147f1527fb805ea5739bd0cd2efd91';
 
 $getAthlete = new CurlSettings('athletes/17138502/stats', array(
     'Content-Type: application/json', 'Authorization: Bearer ' . $authToken));
